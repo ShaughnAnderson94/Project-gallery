f4571a050f9e1e54b94191b2cdf4c36ff24e26a9
ShaughnAnderson94 <andersji94@gmail.com>
2023-02-14 13:44:27 +0000

    meets checklist

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+let myButton = document.querySelector("button");
+const myDescription = document.querySelector("description5");
+let description5 ="this project is under construction";
+myDescription.textContent = "<dZZ>";
+myButton.onclick = () => {
+    console.log("button worked");
+myDescription.textContent = "dZZ";
+}
